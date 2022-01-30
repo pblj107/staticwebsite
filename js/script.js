b239e8f45869784370eddde1a49427886d82350a
Juan Pablo Mejía <juan.mejiab@upb.edu.co>
2022-01-30 08:51:50 -0500

    Actualizacion contacto

---
+++ js/script.js
@@ -0,0 +1,70 @@
+//get data
+const nameImput =document.querySelector("#name");
+const email =document.querySelector("#email");
+const message =document.querySelector("#message");
+const success =document.querySelector("#success");
+const errorNodes =document.querySelectorAll(".error");
+
+
+//validate data
+function validateForm(){
+
+	clearMessages();
+	let errorFlag = false;
+
+	if(nameImput.value.length <1){
+		errorNodes[0].innerText = "Name cannot be blank";
+		nameImput.classList.add("error-border");
+		errorFlag = true;
+
+	}
+
+	if(!emailIsValid(email.value)){
+		errorNodes[1].innerText = "Invalid email address";
+		email.classList.add("error-border");
+		errorFlag = true;
+
+	}
+
+	if(message.value.length <1){
+
+		errorNodes[2].innerText = "Please enter message";
+		message.classList.add("error-border");
+		errorFlag = true;
+	}
+
+
+
+	if(!errorFlag){
+		success.innerText = "Succes!";
+	}
+
+}
+
+//clear error / succes messages
+
+function clearMessages(){
+
+	for(let i =0; i < errorNodes.length; i++){
+
+		errorNodes[i].innerText = "";
+	}
+
+	
+	nameImput.classList.remove("error-border");
+	email.classList.remove("error-border");
+	
+	
+}
+
+// check if email js valid
+
+function emailIsValid(email){
+	let pattern = /\S+@\S+\.\S+/;
+
+	return pattern.test(email);
+
+}
+
+
+
